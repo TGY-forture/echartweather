f97843445284a25ad3b378c82fe76fb57de6198a
TGY-forture <1981472184@qq.com>
2020-11-15 21:52:08 +0800

    增加数据更新功能

--- src/components/airRate.vue
+++ src/components/airRate.vue
@@ -11,18 +11,35 @@ import "../assets/js/chalk";
 import { mapGetters } from "vuex";
 export default Vue.extend({
   name: "Air",
+  data() {
+    return {
+      chart: null,
+      timer: null,
+    };
+  },
   computed: {
     ...mapGetters(["airRate"]),
   },
+  watch: {
+    airRate: function () {
+      if (this.timer) {
+        clearTimeout((this as any).timer);
+      }
+      (this as any).timer = setTimeout(() => {
+        this.updateChart();
+      }, 1000);
+    },
+  },
   mounted() {
-    setTimeout(() => {
-      this.init();
-    }, 3000);
+    this.init();
   },
   methods: {
     init() {
       let el = this.$refs.airtype;
-      let mychart = echarts.init(el, "chalk");
+      this.chart = echarts.init(el, "chalk");
+      let legendtext = this.airRate.map((item: any) => {
+        return item.name;
+      });
       let option = {
         title: {
           show: true,
@@ -33,19 +50,52 @@ export default Vue.extend({
         grid: {
           show: false,
         },
+        tooltip: {
+          trigger: "item",
+        },
         series: [
           {
             type: "pie",
             data: this.airRate,
+            radius: ["50%", "75%"],
+            itemStyle: {
+              borderWidth: 5,
+              borderColor: "#222733",
+            },
+            tooltip: {
+              formatter: "天气类型:{b}<br/>占比:{d}%",
+            },
           },
         ],
-        // legend: {
-        //   show: true,
-        //   icon: "circle",
-        //   data: ["湿度"],
-        // },
+        legend: {
+          show: true,
+          icon: "circle",
+          data: legendtext,
+          orient: "vertical",
+          right: 24,
+          top: "20%",
+        },
       };
-      mychart.setOption(option);
+      (this as any).chart.setOption(option);
+    },
+    updateChart() {
+      let legendtext = this.airRate.map((item: any) => {
+        return item.name;
+      });
+      let noption = {
+        series: [
+          {
+            data: this.airRate,
+            tooltip: {
+              formatter: "天气类型:{b}<br/>占比:{d}%",
+            },
+          },
+        ],
+        legend: {
+          data: legendtext,
+        },
+      };
+      (this as any).chart.setOption(noption);
     },
   },
 });
